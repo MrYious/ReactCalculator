14bc0343021cbe634e9936e5fabb61869b2988fa
MrYious <rosariomark37@gmail.com>
2024-03-20 21:56:25 +0800

    refactor and clean main component

---
+++ ReactCalculator.tsx
@@ -0,0 +1,17 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const ReactCalculator = () => {
+
+  return (
+    <View style={{flex: 1}}>
+      <View style={{flex: 2, backgroundColor: '#193441'}}></View>
+      <View style={{flex: 8, backgroundColor: '#3E606F'}}></View>
+    </View>
+  );
+}
+
+export default ReactCalculator;
