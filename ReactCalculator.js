302a8c6843f9ec905ed7a62de07100de5eab2d88
MrYious <rosariomark37@gmail.com>
2024-03-21 19:07:10 +0800

    add functionality to decimal button, update clear buttons

--- ReactCalculator.js
+++ ReactCalculator.js
@@ -16,7 +16,10 @@ const ReactCalculator = () => {
 
   const [previousInputValue, setPreviousInputValue] = useState(0);
   const [inputValue, setInputValue] = useState(0);
-  const [selectedSymbol, setSelectedSymbol] = useState(null)
+  const [inputDecimalValue, setInputDecimalValue] = useState('');
+  const [selectedSymbol, setSelectedSymbol] = useState(null);
+  const [isDecimal, setIsDecimal] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('');
 
   const renderInputButtons = () => {
     let views = [];
@@ -45,6 +48,10 @@ const ReactCalculator = () => {
   }
 
   const onInputButtonPressed = (input) => {
+    if (errorMessage) {
+      setErrorMessage('');
+    }
+
     switch (typeof input) {
       case 'number':
         return handleNumberInput(input);
@@ -54,7 +61,11 @@ const ReactCalculator = () => {
   }
 
   const handleNumberInput = (num) => {
-    setInputValue((prev) => (prev * 10) + num);
+    if (!isDecimal) {
+      setInputValue((prev) => (prev * 10) + num);
+    } else {
+      setInputDecimalValue((prev) => prev + num);
+    }
   }
 
   const handleStringInput = (str) => {
@@ -63,33 +74,88 @@ const ReactCalculator = () => {
       case '*':
       case '+':
       case '-':
+        const newPreviousInputValue = parseFloat(inputValue + '.' + inputDecimalValue)
+        console.log("Hold: " + newPreviousInputValue);
+        setPreviousInputValue(newPreviousInputValue);
         setSelectedSymbol(str);
-        setPreviousInputValue(inputValue);
         setInputValue(0);
+        setIsDecimal(false);
+        setInputDecimalValue('');
         break;
+
       case '=':
-        if (!selectedSymbol) {
-          return;
+        if (selectedSymbol) {
+          if ( selectedSymbol === '/' && inputValue === 0 && parseInt(inputDecimalValue || 0) === 0 ) {
+            handleStringInput('CE');
+            setErrorMessage('Cannot Divide by Zero');
+            return;
+          }
+
+          const newInputValue = parseFloat(inputValue + '.' + inputDecimalValue)
+          const result = eval(previousInputValue + selectedSymbol + newInputValue);
+          console.log("Current: " + newInputValue);
+          console.log("Result: " + result);
+
+          if (!Number.isInteger(result)) {
+            const values = result.toString().split('.');
+            setIsDecimal(true);
+            setInputValue(parseInt(values.at(0)));
+            setInputDecimalValue(values.at(1));
+          } else {
+            setIsDecimal(false);
+            setInputValue(result);
+            setInputDecimalValue('');
+          }
+
+          setPreviousInputValue(0);
+          setSelectedSymbol(null);
         }
-        setPreviousInputValue(0);
-        setInputValue(eval(previousInputValue + selectedSymbol + inputValue));
-        setSelectedSymbol(null);
         break;
+
+      case '.':
+        if (isDecimal && inputDecimalValue === '') {
+          setIsDecimal(false);
+        } else if (!isDecimal) {
+          setIsDecimal(true);
+        }
+        break;
+
       case 'C':
-        setInputValue((prev) => (prev - (prev % 10)) / 10);
+        if (isDecimal) {
+          if (inputDecimalValue) {
+            const newInputDecimalValue = inputDecimalValue.toString().slice(0, -1);
+            setInputDecimalValue(newInputDecimalValue || '');
+          } else {
+            setIsDecimal(false);
+          }
+        } else if (inputValue !== 0) {
+          const newInputValue = parseInt(inputValue.toString().slice(0, -1));
+          setInputValue(newInputValue || 0);
+        }
         break;
+
       case 'CE':
         setPreviousInputValue(0);
         setInputValue(0);
         setSelectedSymbol(null);
+        setIsDecimal(false);
+        setInputDecimalValue('');
+        setErrorMessage('')
         break;
-   }
+    }
   }
 
   return (
     <View style={Style.rootContainer}>
       <View style={Style.displayContainer}>
-        <Text style={Style.displayText}>{inputValue}</Text>
+        <Text style={Style.displayText}>
+          {inputValue + (isDecimal && '.' + inputDecimalValue)}
+        </Text>
+        {
+          errorMessage && <Text style={Style.displayErrorMessage}>
+            {errorMessage}
+          </Text>
+        }
       </View>
       <View style={Style.inputContainer}>
         {renderInputButtons()}
